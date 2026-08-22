9b29f0d92ffd7c124809cb0aa3c6b536834f22c9
huzriz1 <smhuzaifa956@gmail.com>
2026-08-23 00:08:06 +0500

    feat: add vercel multi-service config and updated api.js base url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // Ek central instance jo aapke port 3000 ke backend se baat karega
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: '/api',
+
   headers: {
     'Content-Type': 'application/json',
   },
